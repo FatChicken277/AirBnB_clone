85cc78861d4fd2e57e801ea53d0a1053d0f0b393
Katorea132 <katorea132@gmail.com>
2020-06-29 18:35:53 -0500

    Added state.py

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module contains a State class.
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Define the State class that inherits from BaseModel.
+
+    Args:
+        BaseModel (class): Base Model class.
+    """
+    name = ""
